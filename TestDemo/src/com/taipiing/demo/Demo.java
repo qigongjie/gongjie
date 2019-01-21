23e1dfd9a7422a00b9a43393b98d49de0230ef86
qigongjie <m17865131892@163.com>
2019-01-21 10:30:26 +0800

    戚功杰:我增加了作者信息或输出了一句话

--- TestDemo/src/com/taipiing/demo/Demo.java
+++ TestDemo/src/com/taipiing/demo/Demo.java
@@ -1,5 +1,8 @@
 package com.taipiing.demo;
 
+/**
+ * @author 12261
+ */
 public class Demo {
 
 	public static void main(String[] args) {
@@ -7,5 +10,6 @@ public class Demo {
 		int q = 4;
 		int w = i + q ;
 		System.out.println(w);
+		System.out.printf("首次提交代码");
 	}
 }
